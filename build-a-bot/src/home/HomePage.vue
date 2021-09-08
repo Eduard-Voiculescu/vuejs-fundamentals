be5ef74bec3b1ddeb2da17a73d4d0b914a1533cc
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2021-09-08 19:28:50 -0400

    Adding gitignore and build a bot

---
+++ build-a-bot/src/home/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" aria-hidden="true" />
+      <div class="get-started">
+        <router-link to="/build">Get started</router-link> building your first robot!
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only (this section is also optional) -->
+<style scoped>
+.home {
+  text-align: center;
+}
+.robot {
+  height: 300px;
+}
+</style>
---
+++ build-a-bot/src/router/index.js
@@ -0,0 +1,66 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import BrowseParts from '../parts/BrowseParts.vue';
+import HomePage from '../home/HomePage.vue';
+import PartInfo from '../parts/PartInfo.vue';
+import SidebarStandard from '../sidebars/SidebarStandard.vue';
+import SidebarBuild from '../sidebars/SidebarBuild.vue';
+import RobotBuilder from '../build/RobotBuilder.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotBases from '../parts/RobotBases.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history', // make sure we set up the server to always return the main index.html file whenever you load any URL
+  routes: [{
+    path: '/',
+    name: 'Home',
+    components: {
+      default: HomePage,
+      sidebar: SidebarStandard,
+    },
+  }, {
+    path: '/build',
+    name: 'Build',
+    components: {
+      default: RobotBuilder,
+      sidebar: SidebarBuild,
+    },
+  }, {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    component: BrowseParts,
+    children: [
+      {
+        path: 'arms',
+        name: 'BrowseArms',
+        component: RobotArms,
+      }, {
+        path: 'bases',
+        name: 'BrowseBases',
+        component: RobotBases,
+      }, {
+        path: 'heads',
+        name: 'BrowseHeads',
+        component: RobotHeads,
+      }, {
+        path: 'torsos',
+        name: 'BrowseTorsos',
+        component: RobotTorsos,
+      },
+    ],
+  }, {
+    path: '/parts/:partType/:id',
+    name: 'Parts',
+    component: PartInfo,
+    props: true,
+    beforeEnter(to, from, next) {
+      const isValidId = Number.isInteger(Number(to.params.id));
+      next(isValidId);
+    },
+  }],
+});
